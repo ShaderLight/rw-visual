ff8748858d5a50892c76e33e512c6e85ba7b8b1b
ShaderLight <37264318+ShaderLight@users.noreply.github.com>
2019-11-17 15:47:55 +0100

    Update rw_visual.py

--- rw_visual.py
+++ rw_visual.py
@@ -13,9 +13,6 @@ while True:
 
     plt.scatter(rw.x_values, rw.y_values,c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=10)
     
-    plt.scatter(rw.x_values[-1], rw.y_values[-1], c="red", edgecolors="none", s=100)
-    plt.scatter(0,0, c='green', edgecolors='none', s=100)
-
     plt.axes().get_xaxis().set_visible(False)
     plt.axes().get_yaxis().set_visible(False)
     
